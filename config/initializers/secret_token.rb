5431823a5a825280714036df6331ad0eee97fd9b
Andrew Chen <sirchen@gmail.com>
2014-12-24 18:21:00 -0800

    Create Articles model, controller, index and show views, add CSS file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = 'd728080dcd2d843d6f8fd25465b2d08cd233f6175c4dd0f279f75999dab5ad3baa1afb2d3ae3b0f281304ef5e39f3e22dc34606cb0bb2474b3706d1ffbb96f66'
