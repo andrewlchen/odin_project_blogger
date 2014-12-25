37f3117142ed95d85d627e02fa10f935a672b5f7
Andrew Chen <sirchen@gmail.com>
2014-12-24 20:58:34 -0800

    Add basic CRUD operations, add flash messages for user feedback

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,44 @@ class ArticlesController < ApplicationController
 	def show 
 		@article = Article.find(params[:id])
 	end  
+
+	def new 
+		@article = Article.new 
+	end 
+
+	def create 
+		@article = Article.new(article_params)
+		@article.save 
+
+		flash.notice = "New post created: '#{@article.title}'"
+
+		redirect_to article_path(@article)
+	end 
+
+	def destroy 
+		@article = Article.find(params[:id])
+		@article.destroy 
+
+		flash.notice = "Post '#{@article.title}' was deleted."
+
+		redirect_to articles_path
+	end 
+
+	def edit 
+		@article = Article.find(params[:id])
+	end 
+
+	def update 
+		@article = Article.find(params[:id])
+		@article.update(article_params) 
+
+		flash.notice = "Post '#{@article.title}' was updated."
+
+		redirect_to article_path(@article)
+	end 
+
+	private 
+		def article_params
+			params.require(:article).permit(:title, :body)
+		end 
 end
