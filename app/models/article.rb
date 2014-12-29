102d1c10cf40a65bb17bc39564c5b0266b4ba8dc
Andrew Chen <sirchen@gmail.com>
2014-12-28 16:33:42 -0800

    Add tagging feature, with ability to click from article to tag and back, plus view list of all tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,18 @@
 class Article < ActiveRecord::Base
 	has_many :comments
+	has_many :taggings
+	has_many :tags, through: :taggings 
+
+	def tag_list 
+		tags.join(", ")
+	end 
+
+	def tag_list=(tags_string)
+		tag_list = tags_string.split(",")
+		tag_list = tag_list.collect{|word| word.strip.downcase }
+		unique_tags = tag_list.uniq 
+		new_or_found_tags = unique_tags.collect{ |word| Tag.find_or_create_by(name: word) }
+		self.tags = new_or_found_tags
+	end 
+
 end
